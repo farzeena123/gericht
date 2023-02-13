51d8e9fed87591111eceb3095375d07bdc8179a2
farzeena123 <farzeena2000@gmail.com>
2023-02-13 18:45:36 +0300

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,60 @@
+
+
+const drinks = [
+  {
+    title: 'Vanilla ',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Orange Juice',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Banana Shake ',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Chocolate Shake ',
+    price: '$31',
+    tags: 'CA | 750 ml',
+  },
+  {
+    title: 'CherryVanilla ',
+    price: '$26',
+    tags: 'IE | 750 ml',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Aperol Sprtiz',
+    price: '$20',
+    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+  },
+  {
+    title: "Dark 'N' Stormy",
+    price: '$16',
+    tags: 'Dark rum | Ginger beer | Slice of lime',
+  },
+  {
+    title: 'Daiquiri',
+    price: '$10',
+    tags: 'Rum | Citrus juice | Sugar',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$31',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: 'Negroni',
+    price: '$26',
+    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+  },
+];
+
+
+export default { drinks, cocktails};
